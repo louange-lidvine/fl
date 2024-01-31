5a4575d97a5d77fc1248821c99d84c5319574a86
louange-lidvine <lidvinelouange@gmail.com>
2024-01-31 08:52:54 +0200

    .added a navbar at the bottom

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,11 @@
-
-
 import 'package:flutter/material.dart';
 import '../constants/colors.dart';
 import '../widgets/todo_item.dart';
 import '../model/todo.dart';
+import '../widgets/bottom-nav.dart';
+
+// Import BottomNavigationExample here
+import '../widgets/bottom-nav.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -16,6 +18,7 @@ class _HomeState extends State<Home> {
   final todosList = Todo.todoList();
   List<Todo> _foundTodo = [];
   final _todoController = TextEditingController();
+
   @override
   void initState() {
     _foundTodo = todosList;
@@ -25,9 +28,10 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: tdBGColor,
-        appBar: _buildAppBar(),
-        body: Stack(children: [
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Stack(
+        children: [
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             child: Column(
@@ -46,14 +50,12 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                       ),
-
                       for (Todo todoo in _foundTodo.reversed)
                         TodoItem(
                           todo: todoo,
                           onTodoChanged: _handleToDoChange,
                           onDeleteItem: _deleteTodoChange,
-                        )
-//ake sure ToDoItem is wrapped inside a column or any other widget
+                        ),
                     ],
                   ),
                 ),
@@ -62,58 +64,71 @@ class _HomeState extends State<Home> {
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: Row(children: [
-              Expanded(
+            child: Row(
+              children: [
+                Expanded(
                   child: Container(
-                margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    const BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(0.0, 0.0),
-                        blurRadius: 10.0,
-                        spreadRadius: 0.0),
-                  ],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextField(
-                  controller: _todoController,
-                  decoration: InputDecoration(
-                      hintText: 'Add new todo item', border: InputBorder.none),
-                ),
-              )),
-              Container(
-                margin: EdgeInsets.only(
-                  bottom: 20,
-                  right: 20,
-                ),
-                child: ElevatedButton(
-                  child: Text(
-                    '+',
-                    style: TextStyle(fontSize: 40, color: Colors.white),
-                  ),
-                  onPressed: () {
-                    _addToDoItem(_todoController.text);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: tdBlue,
-                    // minimumSize: Size(60,60),
-                    elevation: 10,
+                    margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        const BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                        hintText: 'Add new todo item',
+                        border: InputBorder.none,
+                      ),
+                    ),
                   ),
                 ),
-              )
-            ]),
-          )
-        ]));
+                Container(
+                  margin: EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    child: Text(
+                      '+',
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _addToDoItem(_todoController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: tdBlue,
+                      elevation: 10,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      // Add BottomNavigationExample here
+      bottomNavigationBar: BottomNavigationExample(
+        onTabChanged: (index) {
+          // Handle tab change
+          if (index == 0) {
+            // If "Home" tab is selected, navigate to the Home screen
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Home()),
+            );
+          }
+          // Add more conditions for other tabs if needed
+        },
+      ),
+    );
   }
 
-  // void _handleToDoChange(Todo todo) {
-  //   setState(() {
-  //     todo.isDone = !todo.isDone;
-  //   });
-  // }
   void _handleToDoChange(Todo todo) {
     setState(() {
       todo.isDone = !todo.isDone;
@@ -132,7 +147,6 @@ class _HomeState extends State<Home> {
           id: DateTime.now().millisecondsSinceEpoch.toString(),
           todoText: todo));
     });
-
 
     _todoController.clear();
   }
@@ -155,7 +169,6 @@ class _HomeState extends State<Home> {
 
   Widget searchBox() {
     return Container(
-    
       padding: EdgeInsets.symmetric(horizontal: 15),
       decoration: BoxDecoration(
         color: Colors.white,
--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -59,5 +59,6 @@ class TodoItem extends StatelessWidget {
         ),
       ),
     );
+ 
   }
 }
